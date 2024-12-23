817fdbfe3b5eac74fee8a5f42bc18a1c0c5a49ab
iracleous <iracleous@gmail.com>
2024-12-23 14:59:47 +0200

    db customer

--- AccEshop/Models/EshopDbContext.cs
+++ AccEshop/Models/EshopDbContext.cs
@@ -14,6 +14,10 @@ public class EshopDbContext:DbContext
     public DbSet<Order> Orders{ get; set; }
     public DbSet<OrderProduct> OrderProducts{ get; set; }
 
+    public EshopDbContext(DbContextOptions<EshopDbContext> options): base(options)
+    {
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         // Set up your connection string and other configuration here
